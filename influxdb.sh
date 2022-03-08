20f363278787f1de8c931e5b925bb3dcade57e51
MeleshK <51149294+MeleshK@users.noreply.github.com>
2022-03-08 21:35:46 +0000

    Update influxdb.sh

--- influxdb.sh
+++ influxdb.sh
@@ -197,7 +197,7 @@ echo -e "${CM}${CL} \r"
 
 alias lxc-cmd="lxc-attach -n $CTID --"
 
-lxc-cmd bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/setup/influxdb-install.sh)" || exit
+lxc-cmd bash -c "$(wget -qLO - https://raw.githubusercontent.com/MeleshK/Proxmox/main/influxDB_install.sh)" || exit
 
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
 
